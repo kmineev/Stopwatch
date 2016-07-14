f8a053b2908180c12422f5a662e9486e94dc3772
kmineev <kostelom70@gmail.com>
2016-07-14 16:12:54 +0700

    Remark
    Remark

---
+++ app/src/main/java/com/spaceotechnologies/training/stopwatch/CounterFragment.java
@@ -0,0 +1,21 @@
+package com.spaceotechnologies.training.stopwatch;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kostez on 13.07.2016.
+ */
+public class CounterFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.fragment_counter, container, false);
+
+        return rootView;
+    }
+}
